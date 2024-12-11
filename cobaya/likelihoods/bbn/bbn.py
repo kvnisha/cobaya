2fe4b857cc77e595b327add0fdf3ef5b691ed382
kvnisha <nishavk@utdallas.edu>
2024-12-11 17:07:34 -0600

    Adding BBN Likelihood

---
+++ cobaya/likelihoods/bbn/bbn.py
@@ -0,0 +1,17 @@
+import numpy as np
+from cobaya.likelihood import Likelihood
+
+class BBN(Likelihood):
+    # Data type for aggregrated chi2
+    type = "bbn"
+
+    def initialize(self):
+        self.mean = np.ravel(self.mean)
+        self.invcov = np.linalg.inv(np.atleast_2d(self.cov))
+
+    def get_requirements(self):
+        return {name: None for name in self.quantities}
+
+    def logp(self, **params_values):
+        diff = self.mean -np.array([self.provider.get_param(name) for name in self.quantities])
+        return -0.5 * diff.dot(self.invcov).dot(diff)
---
+++ cobaya/likelihoods/bbn/schoneberg2024.py
@@ -0,0 +1,6 @@
+from .bbn import BBN
+
+class schoneberg2024(BBN):
+    
+    r"""BBN: omega_b measurement from https://arxiv.org/abs/2401.15054: The 2024 BBN baryon abundance update
+    """
